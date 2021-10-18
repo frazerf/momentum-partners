d08b0c649d07c0a166e845a2556f581c34778a99
Frazer Findlater <frazer.findlater@gmail.com>
2021-10-18 16:00:24 +1300

    Adding dist folder.

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+"use strict";console.log("Testing JS import file one."),$("#toggle").click(function(){$(this).toggleClass("active"),$("#overlay").toggleClass("open")});var rellax=new Rellax(".rellax");console.log("Testing JS import file two.");
