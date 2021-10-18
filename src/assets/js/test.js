95632e631b4c351b2687868eeac1e9f8822fd8e3
Frazer Findlater <frazer.findlater@gmail.com>
2021-10-18 13:20:53 +1300

    Testing netlify build.

--- src/assets/js/test.js
+++ src/assets/js/test.js
@@ -1,9 +1,9 @@
 console.log("Testing JS import file one.");
 // Test JavaScript, to ensure Babel is working.
 
-const array1 = ['a', 'b', 'c'];
+$('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+});
 
-array1.forEach(element => console.log(element));
-
-const matches = document.querySelectorAll("div");
-console.log(matches);
+var rellax = new Rellax(".rellax");
